5a6ab7b280b3ade35741c1df20decbfd7e6da532
Ivan Kashtanov <iakashtanov@gmail.com>
2024-12-31 02:55:28 +0400

    feat: add placeholder generation to dummy resolver

--- KnowledgeBaseService/Infrastructure.DummyResolver/DummyKnowledgeBaseEntryResolver.cs
+++ KnowledgeBaseService/Infrastructure.DummyResolver/DummyKnowledgeBaseEntryResolver.cs
@@ -9,10 +9,21 @@ public class DummyKnowledgeBaseEntryResolver : IFlatKnowledgeBaseEntryResolver
         CancellationToken cancellationToken)
     {
         return Task.FromResult(entrySpecs
-            .Select(es => es.EntryKey.Contains("missing", StringComparison.InvariantCultureIgnoreCase)
-                ? null
-                : new FlatKnowledgeBaseEntry(es.EntryKey, $"Resolved {es.EntryKey}"))
+            .Select(ResolveEntrySpec)
             .Where(e => e is not null)
             .ToDictionary(es => es!.EntryKey, es => es!));
+    }
+
+    private static FlatKnowledgeBaseEntry? ResolveEntrySpec(FlatKnowledgeBaseEntrySpecification entrySpec)
+    {
+        return entrySpec.EntryKey switch
+        {
+            { } missingKey when missingKey.Contains("missing", StringComparison.InvariantCultureIgnoreCase) => null,
+            { } placeholderKey when placeholderKey.Contains("placeholder", StringComparison.InvariantCultureIgnoreCase)
+                =>
+                new FlatKnowledgeBaseEntry(placeholderKey,
+                    $"Resolved with placeholder [[placeholder]] for key {placeholderKey}"),
+            { } key => new FlatKnowledgeBaseEntry(key, $"Resolved for key {key}")
+        };
     }
 }
